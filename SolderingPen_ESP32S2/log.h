5663fb67394163d5e309cfd5c0c5da923244f73f
Emil Muratov <gpm@hotplug.ru>
2024-03-21 22:43:25 +0900

    remove spaces in LOG macro

--- SolderingPen_ESP32S2/log.h
+++ SolderingPen_ESP32S2/log.h
@@ -34,13 +34,13 @@ LOG macro will enable/disable logs to serial depending on LAMP_DEBUG build-time 
 #endif
 
 #if defined(PTS200_DEBUG_LEVEL) && PTS200_DEBUG_LEVEL > 3
-	#define LOGD(tag, func, ...) PTS200_DEBUG_PORT.print(" D: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
+	#define LOGD(tag, func, ...) PTS200_DEBUG_PORT.print("D: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
 #else
 	#define LOGD(...)
 #endif
 
 #if defined(PTS200_DEBUG_LEVEL) && PTS200_DEBUG_LEVEL > 2
-	#define LOGI(tag, func, ...) PTS200_DEBUG_PORT.print(" I: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
+	#define LOGI(tag, func, ...) PTS200_DEBUG_PORT.print("I: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
 	// compat macro
 	#define LOG(func, ...) PTS200_DEBUG_PORT.func(__VA_ARGS__)
 #else
@@ -50,13 +50,13 @@ LOG macro will enable/disable logs to serial depending on LAMP_DEBUG build-time 
 #endif
 
 #if defined(PTS200_DEBUG_LEVEL) && PTS200_DEBUG_LEVEL > 1
-	#define LOGW(tag, func, ...) PTS200_DEBUG_PORT.print(" W: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
+	#define LOGW(tag, func, ...) PTS200_DEBUG_PORT.print("W: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
 #else
 	#define LOGW(...)
 #endif
 
 #if defined(PTS200_DEBUG_LEVEL) && PTS200_DEBUG_LEVEL > 0
-	#define LOGE(tag, func, ...) PTS200_DEBUG_PORT.print(" E: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
+	#define LOGE(tag, func, ...) PTS200_DEBUG_PORT.print("E: "); PTS200_DEBUG_PORT.print(tag); PTS200_DEBUG_PORT.print("\t"); PTS200_DEBUG_PORT.func(__VA_ARGS__)
 #else
 	#define LOGE(...)
 #endif
